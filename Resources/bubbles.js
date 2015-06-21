3adbdccb7fdf0f87bda6f2e281f228362f30cfa4
Irraquated <server.nimbus@gmail.com>
2015-06-21 17:10:34 +1000

    Update bubbles.js

--- Resources/bubbles.js
+++ Resources/bubbles.js
@@ -156,7 +156,7 @@ function initEventListeners() {
  
 function updateCanvasDimensions() {
     canvas.attr({
-        height: 700,
+        height: 650,
         width: 1200
     });
     canvasWidth = canvas.width();
